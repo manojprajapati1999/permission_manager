6841d0e5bc0f8137a8c06a1cbc4c78339185bddd
manojprajapati1999 <patadiyamanoj4@gmail.com>
2026-01-17 14:45:35 +0530

    Add files via upload

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import permission_manager
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PermissionManagerPlugin.register(with: registry.registrar(forPlugin: "PermissionManagerPlugin"))
+}
---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <permission_manager/permission_manager_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  PermissionManagerPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PermissionManagerPlugin"));
+}
